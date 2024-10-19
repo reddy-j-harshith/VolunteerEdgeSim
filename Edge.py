5d35454a6e9190422be2f062a28dcb52f2e04916
Yousef S. Alsenani <115349418+SimuEnv@users.noreply.github.com>
2024-10-19 17:47:11 +0300

    Delete Edge.py

--- Edge.py
+++
@@ -1,8 +0,0 @@
-from GeneralUnit import GeneralUnit
-
-class Edge(GeneralUnit):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    def getType(self):
-        return 'edge'
